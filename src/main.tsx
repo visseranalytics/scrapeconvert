56c5ed5a1bcd272b2a00a5600f5100b7249bd811
Visser Analytics <timothy@visseranalytics.com>
2025-11-25 19:01:42 -0600

    Implemented tailwind correctly

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
